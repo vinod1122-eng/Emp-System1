ecfa9c5c05d563a4af43dab90f062a5cb8c2fad1
vinod <vinodrajput8757@gmail.com>
2023-05-27 20:57:15 +0530

    Initial commit

---
+++ src/main/java/com/prog/EmpSystem1Application.java
@@ -0,0 +1,14 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmpSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSystem1Application.class, args);
+	}
+
+}
